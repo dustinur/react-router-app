208dce811fc0df9f9919c8c540752b769aa435fb
Dustin Robertson <dustinuriah@gmail.com>
2017-09-23 23:57:09 -0600

    Fake Auth & Dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { isAuthenticated } from '../fakeAuth';
+
+const Dashboard = () => {
+  if (isAuthenticated()) {
+    return (
+      <div>
+        <h3>You are logged in!</h3>
+      </div>
+    )
+  } else {
+    return <Redirect to="/login" />
+  }
+}
+
+
+export default Dashboard;
